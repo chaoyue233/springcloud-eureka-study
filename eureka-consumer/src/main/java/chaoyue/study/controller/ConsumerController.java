54f6219690592c521144f57cfbcfb3970708e463
chaoyue233 <beberoy@sina.com>
2024-05-28 23:03:25 +0800

    调整部分配置和注释

--- eureka-consumer/src/main/java/chaoyue/study/controller/ConsumerController.java
+++ eureka-consumer/src/main/java/chaoyue/study/controller/ConsumerController.java
@@ -18,17 +18,18 @@ public class ConsumerController {
 
     @GetMapping("test")
     public String test() {
-        //服务发现
+        // 通过服务名获取服务信息
         List<ServiceInstance> instances = discoveryClient.getInstances("EUREKA-PROVIDER");
         if (instances == null || instances.size() <= 0) {
             return null;
         }
-        //通过某个策略拿到一个实例
+        // 获取其中的一个服务实例进行调用
         ServiceInstance serviceInstance = instances.get(0);
         String host = serviceInstance.getHost();
         int port = serviceInstance.getPort();
         System.out.println(host);
         System.out.println(port);
+        // 拿到IP和端口之后拼装成地址进行REST接口调用
         String url = "http://" + host + ":" + port + "/providerFunction";
         String result = restTemplate.getForObject(url, String.class);
         return "consumer: " + result;
--- eureka-provider/src/main/java/chaoyue/study/EurekaProviderApplication.java
+++ eureka-provider/src/main/java/chaoyue/study/EurekaProviderApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+/**
+ * 项目启动后访问 http://localhost:8761/ 即可进入eureka控制台
+ */
 @SpringBootApplication
 @EnableEurekaClient
 public class EurekaProviderApplication {
