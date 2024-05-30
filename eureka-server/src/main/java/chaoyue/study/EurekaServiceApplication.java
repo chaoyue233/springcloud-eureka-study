c45d96e54721c95cbba404fb059e12e18b091c20
chaoyue <beberoy@sohu.com>
2024-05-30 17:33:21 +0800

    调整注释内容

--- eureka-server/src/main/java/chaoyue/study/EurekaServiceApplication.java
+++ eureka-server/src/main/java/chaoyue/study/EurekaServiceApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * 项目启动后访问 http://localhost:8761/ 即可进入eureka控制台
+ */
 @SpringBootApplication
 @EnableEurekaServer // 导入EurekaServerMarkerConfiguration自动配置类
 public class EurekaServiceApplication {
